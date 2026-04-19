2836a2fff8ecccbab91f095abb89bfc96f32fcd5
maheshkumar09104 <mahesh09104@gmail.com>
2026-04-19 10:28:07 +0530

    fix the error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
@@ -24,6 +24,7 @@ app.use("/api/feedback", feedbackRoutes);
 
 app.get("/", (req, res) => res.send("Saffron API running ✅"));
 
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
